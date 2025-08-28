067948cd81a28398d48bd13183ed5c9cf90bf266
Federico Morsia <fede28.morsia@gmail.com>
2025-08-28 17:45:01 +0200

    b
    Signed-off-by: Federico Morsia <fede28.morsia@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import "./global.css";
+
+import { Merriweather } from 'next/font/google'
+
+const merriweather = Merriweather({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function RootLayout({
   children,
@@ -7,8 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-red-400">{children}
+    <html lang="en" className={merriweather.className}>
+      <body className="max-w-3xl mx-auto p-4">
+      <main>{children}</main>
       <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
