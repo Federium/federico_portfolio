b3baa908cf18bcc0655d89747977bd0251c9e9b4
Federico Morsia <fede28.morsia@gmail.com>
2025-09-18 18:37:56 +0200

    robette
    Signed-off-by: Federico Morsia <fede28.morsia@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
-import "./global.css";
 
-import { Merriweather } from 'next/font/google'
 
-const merriweather = Merriweather({ subsets: ['latin'], weight: ['400', '700'] })
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'], weight: ['700'] })
 
 export default function RootLayout({
   children,
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={merriweather.className}>
-      <body className="max-w-3xl mx-auto p-4">
+    <html lang="en" className={montserrat.className}>
+      <body>
       <main>{children}</main>
       <PrismicPreview repositoryName={repositoryName} />
       </body>
--- src/slices/RichText/index.tsx
+++ src/slices/RichText/index.tsx
@@ -1,35 +1,16 @@
-import { type FC } from "react";
-import { type Content } from "@prismicio/client";
+import { FC } from "react";
+import { Content } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
-import {
-  PrismicRichText,
-  type SliceComponentProps,
-  type JSXMapSerializer,
-} from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
-const components: JSXMapSerializer = {
-  hyperlink: ({ node, children }) => {
-    return <PrismicNextLink field={node.data}>{children}</PrismicNextLink>;
-  },
-  label: ({ node, children }) => {
-    if (node.data.label === "codespan") {
-      return <code>{children}</code>;
-    }
-  },
-};
 
-/**
- * Props for `RichText`.
- */
+
 type RichTextProps = SliceComponentProps<Content.RichTextSlice>;
 
-/**
- * Component for "RichText" Slices.
- */
 const RichText: FC<RichTextProps> = ({ slice }) => {
   return (
     <section>
-      <PrismicRichText field={slice.primary.content} components={components} />
+      <PrismicRichText field={slice.primary?.content}  />
     </section>
   );
 };
