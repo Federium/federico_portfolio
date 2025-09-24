ef054e2d5bfd509d4121dbd6c765ace506d9c8af
Federico Morsia <fede28.morsia@gmail.com>
2025-09-24 16:33:29 +0200

    Revert "prova deploy"
    This reverts commit 89d38ad4f313e67a12393240d1710fa8998c72e8.

--- next.config.ts
+++ next.config.ts
@@ -2,25 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
-  // Assicura che CSS sia processato correttamente
-  experimental: {
-    optimizeCss: true,
-  },
-  
-  // Configurazione per il CSS processing
-  webpack: (config, { dev }) => {
-    if (!dev) {
-      // In produzione, assicurati che PostCSS processi Tailwind
-      config.optimization.splitChunks.cacheGroups.styles = {
-        name: 'styles',
-        test: /\.(css|scss)$/,
-        chunks: 'all',
-        enforce: true,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
